0abcb149244d75ee609a4c91cbc7cbbaad001799
Daniel Paulino <danielp1469@gmail.com>
2020-03-10 11:42:29 -0300

    Criação do atributo limite, getters and setters do limite, criação dos testes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,24 @@ import exercicios.*;
 
 public class Main {
     public static void main(String[] args) {
+        Conta conta1 = new Conta();
+        conta1.setLimite(1000.00);
+        conta1.setSaldo(500.00);
+        conta1.setNumero(1);
 
+        Conta conta2 = new Conta();
+        conta2.setNumero(2);
+        conta2.setSaldo(1500.00);
+
+        //conta1.sacar(600.00);
+        //conta1.sacar(900.00);
+        //conta1.sacar(100.00);
+
+        conta1.transferir(conta2, 600.00);
+        conta1.transferir(conta2, 900.00);
+        //conta1.transferir(conta2, 100.00);
+
+        System.out.println(conta1.getSaldo());
     }
 
 }
---
+++ src/main/java/exercicios/Conta.java
@@ -0,0 +1,59 @@
+package exercicios;
+
+public class Conta {
+    private Integer numero;
+    private Double saldo;
+    private Double limite;
+
+    public void depositar(Double valor) {
+        saldo = saldo + valor;
+        if (valor <= 0)
+            throw new IllegalArgumentException("Valor deve ser positivo");
+    }
+
+    public void sacar(Double valor) {
+        saldo = saldo - valor;
+
+        if (valor <= 0)
+            throw new IllegalArgumentException("Valor deve ser positivo");
+
+        if (saldo < limite - limite - limite)
+            throw new IllegalArgumentException("Saldo menor que limite");
+    }
+
+    public void transferir(Conta conta, Double valor) {
+        saldo = saldo - valor;
+        conta.saldo = conta.saldo + valor;
+
+        if (valor <= 0)
+            throw new IllegalArgumentException("Valor deve ser positivo");
+
+        if (saldo < limite - limite - limite)
+            throw new IllegalArgumentException("Saldo menor que limite");
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setLimite(Double limite) {
+        this.limite = limite;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Double getLimite() {
+        return limite;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+}
+
